f3dbd2dd3374c49e10535666f8c0b5f7191793b0
Alfred D'Souza <dsouzaalfred@gmail.com>
2023-10-04 18:35:14 +0100

    add fb link

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { HeroImage } from "@/components/HeroImage";
-import { H1, H3 } from "@/components/Typography";
+import { H1, H3, P } from "@/components/Typography";
 
 export default function Home() {
   return (
@@ -9,10 +9,20 @@ export default function Home() {
           Scottish United Konkani Association - (SUKA)
         </H1>
         <H3 className="mt-2 mb-16">More details coming soon</H3>
+        <div>
+          <P className="mt-2 mb-16">
+            Connect with us on our{" "}
+            <a
+              href="https://www.facebook.com/suka.scotland"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+            >
+              FB Page
+            </a>
+          </P>
+        </div>
       </HeroImage>
-      <section className="flex flex-col items-center justify-between p-8">
-        <div>Some section</div>
-      </section>
     </main>
   );
 }
